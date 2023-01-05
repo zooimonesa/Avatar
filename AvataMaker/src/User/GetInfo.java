b6182b07ba10b3e798274e06497fe454b9b97e18
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-05 12:12:59 +0900

    회원정보가 모이는 클래스

---
+++ AvataMaker/src/User/GetInfo.java
@@ -0,0 +1,19 @@
+package User;
+
+public class GetInfo {
+	public static String fmainid;
+	public static String fmainnickname;
+	public static int fmainpoint;
+	public static int fmainhealth;
+	public static int fmainintelligence;
+	public static int fmaintalent;
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(fmainnickname);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
